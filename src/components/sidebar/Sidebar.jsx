78b663c71a30620b8b3f573cb246ec870a6f3559
Gautam-Ghai <gautamghai99@gmail.com>
2021-07-13 16:07:50 -0700

    First commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import "./sidebar.css";
+import { Link } from "react-router-dom";
+
+//Material-UI
+import {
+  Home,
+  Timeline,
+  TrendingUp,
+  PermIdentity,
+  Storefront,
+  AttachMoney,
+  BarChart,
+  MailOutline,
+  DynamicFeed,
+  ChatBubbleOutline,
+  WorkOutlineOutlined,
+  ReportOutlined,
+} from "@material-ui/icons";
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Menu</h3>
+          <ul className="sidebarList">
+            <Link to="/">
+              <li className="sidebarListItem active">
+                <Home className="sidebarIcon" /> Home
+              </li>
+            </Link>
+
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" /> Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUp className="sidebarIcon" /> Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <Link to="/users">
+              <li className="sidebarListItem">
+                <PermIdentity className="sidebarIcon" /> Users
+              </li>
+            </Link>
+            <li className="sidebarListItem">
+              <Storefront className="sidebarIcon" /> Products
+            </li>
+            <li className="sidebarListItem">
+              <AttachMoney className="sidebarIcon" /> Transactions
+            </li>
+            <li className="sidebarListItem">
+              <BarChart className="sidebarIcon" /> Reports
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <MailOutline className="sidebarIcon" /> Mail
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeed className="sidebarIcon" /> Feedback
+            </li>
+            <li className="sidebarListItem">
+              <ChatBubbleOutline className="sidebarIcon" /> Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <WorkOutlineOutlined className="sidebarIcon" /> Manage
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" /> Analytics
+            </li>
+            <li className="sidebarListItem">
+              <ReportOutlined className="sidebarIcon" /> Reports
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import "./widgetSm.css";
+
+//Material-Ui
+import { Visibility } from '@material-ui/icons';
+export default function WidgetSm() {
+    return (
+        <div className="widgetSm">
+            <span className="widgetSmTitle">New Job Members</span>
+            <ul className="widgetSmList">
+                <li className="widgetSmListItem">
+                    <img src="https://source.unsplash.com/random" alt="user" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://source.unsplash.com/random" alt="user" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://source.unsplash.com/random" alt="user" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://source.unsplash.com/random" alt="user" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://source.unsplash.com/random" alt="user" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
+}
